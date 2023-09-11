a9170f739433d00bdfe70a21c89aac88db9822ac
GULAM ASSAD SIDIQIE <cbsv001@gmail.com>
2023-09-11 09:41:21 +0700

    a

--- sw-admin/login/index.php
+++ sw-admin/login/index.php
@@ -63,7 +63,7 @@ if(!empty($_SESSION['SESSION_USER']) && !empty($_SESSION['SESSION_ID'])){
       <div class="col-md-12" style="min-height:40px;"><span id="stat"></span></div>
         <div>
           <button type="submit" class="btn btn-primary btn-block btn-flat" id="login">Login to Admin</button>
-           <a href="https://timkoding.com/downloads/source-code-absensi-online-absen-selfie-deteksi-lokasi-berbasis-web-bonus-source-code-e-book-v3-lock-lokasi/" class="btn btn-warning  btn-block">Beli Aplikasi</a>
+          <!-- <a href="https://timkoding.com/downloads/source-code-absensi-online-absen-selfie-deteksi-lokasi-berbasis-web-bonus-source-code-e-book-v3-lock-lokasi/" class="btn btn-warning  btn-block">Beli Aplikasi</a> -->
         </div>
         <!-- <div class="col-xs-12">
         <button type="submit" class="btn btn-primary btn-block btn-flat" id="login">Login to Admin</button>
